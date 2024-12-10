5170f37724d9bcbd62d0b85ee0c4288c2604ab2e
Txomin <tmedrano@tarragona.cat>
2024-12-10 14:49:08 +0100

    changes composer vrsions

--- src/Models/Requests/BaixaAnimal.php
+++ src/Models/Requests/BaixaAnimal.php
@@ -20,12 +20,15 @@ class BaixaAnimal extends AnicomUpdateRequest
 
         $variables=[
             'data_baixa'=>$data_baixa?$data_baixa:Carbon::now()->format('d/m/Y'),
-            'motiu_baixa'=>$motiu,
-            'acreditacio_mort'=>'S',
+            'motiu_baixa'=>$motiu?$motiu:1,
             'idPk'=>$id_animal."N",
             'nivell'=>46932
         ];
            
+        //si és per mort
+        if($motiu==3){
+            $variables['acreditacio_mort']='S';
+        }
         parent::__construct($variables);
     }
 }
